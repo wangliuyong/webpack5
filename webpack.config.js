b4676203246063d0ba30f2f5e8b5e0b41a612369
wly <1355498705@qq.com>
2021-04-26 22:27:13 +0800

    使用babel-loader  打包js

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,17 @@
 module.exports = {
-  mode:'production'
+  mode:'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
