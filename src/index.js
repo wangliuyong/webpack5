76776ed590c771bb253c3f6d07f02abe8d15616f
wly <1355498705@qq.com>
2021-04-26 22:51:01 +0800

    eslint  修复

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import a from './a' // // 直接引入
-const b = import('./b') // 动态引入，也按需加载
-import jsxDemo from './demo.jsx'
+import a from './a' // 动态引入，也按需加载
+import jsxDemo from './demo.jsx' // // 直接引入
+const b = import('./b')
 
 const fn = () => {
     console.log(a,b)
